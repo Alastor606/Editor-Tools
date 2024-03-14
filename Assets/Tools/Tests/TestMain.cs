2c7d11e7ca90197e3ac43f06a9495c473967ba5d
alastor <114815838+Alastor606@users.noreply.github.com>
2024-03-14 23:15:55 +0300

    Update TestMain.cs

--- Assets/Tools/Tests/TestMain.cs
+++ Assets/Tools/Tests/TestMain.cs
@@ -5,27 +5,26 @@ namespace EditorTools
 
     public class TestMain : EditorWindow
     {
-        int _testInt;
-        float _testFloat;
-        string _testString;
-        GameObject obj;
-        bool _testBool;
+        private string _testString;
+        private GameObject obj;
+        private bool _testBool;
         [MenuItem("Test/Main")]
-        public static void CreateWindow() 
-        {
+        public static void CreateWindow()  =>
             GetWindow<TestMain>("Tester").minSize = new(400, 475);
-        }
+        
 
         private void OnGUI()
         {
-            Draw.Label("Ahaha", 25, Color.green);
-            _testInt.Field("TestInt", 10).Space(50);
-            _testFloat.Field("Test float", 50);
-            Draw.TextField(ref _testString);
-            Draw.ObjectField(ref obj, allowSceneObjects: true);
-            Draw.Button("Chec button", () => Debug.Log("ahah"));
-            _testBool.Field("Test bool", true);
-            
+            Align.Horizontal(() =>
+            {
+                Draw.Label("Test label", 25, Color.green);
+                Draw.TextField(ref _testString);
+                Draw.ObjectField(ref obj, allowSceneObjects: true);
+                Draw.Button("Chec button", () => Debug.Log("Button pressed"));
+                _testBool.Field("Test bool", true);
+
+            });
+           
         }
     }
 }
